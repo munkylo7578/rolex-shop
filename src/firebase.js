49ca4cb51b832a09601ef8bbe1341b36d88b2156
TNC <donam2832000@gmail.com>
2021-11-26 06:53:38 +0700

    fix

--- src/firebase.js
+++ src/firebase.js
@@ -10,13 +10,13 @@ import 'firebase/compat/database/dist/index.esm.js';
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  authDomain: "rolex-shop.firebaseapp.com",
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket:  process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId:  process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId:  process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID 
+  projectId: "rolex-shop",
+  storageBucket:  "rolex-shop.appspot.com",
+  messagingSenderId:  "368639051908",
+  appId:  "1:368639051908:web:678d24b010b3b8cbd2dad1",
+  measurementId: " G-T5NVBRX56W"
   
 };
 
